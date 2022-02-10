ec471bc3168e51018ce0dcc2c1629b699113df58
Dipendra Pratap <dipendrapratap155@gmail.com>
2022-02-09 21:42:33 -0800

    organizing and gates code

--- and.py
+++ and.py
@@ -1,30 +1,34 @@
+from fileinput import filename
 from utils.model import Perceptron
 from utils.all_utils import prepare_data,save_model, save_plot
 import pandas as pd
 import numpy as np
 import joblib
 
-def main():
-    AND ={
-        "x1" : [0,0,1,1],
-        "x2" : [0,1,0,1],
-        "y" : [0,0,0,1]
-    }
+def main(data,eta,epochs,filename,plotfilename):
 
-    df = pd.DataFrame(AND)
+    df = pd.DataFrame(data)
 
     X, y = prepare_data(df)
 
-    ETA = 0.3
-    EPOCHS = 10
+
 
     model  = Perceptron(eta= ETA, epochs= EPOCHS)
     model.fit(X,y)
 
     _= model.total_loss()
 
-    save_model(model=model,filename='and.model')
-    save_plot(df,'and.png',model )
+    save_model(model=model,filename=filename)
+    save_plot(df, file_name=plotfilename,model=model)
 
 if __name__=="__main":
-    main()
+
+    AND ={
+            "x1" : [0,0,1,1],
+            "x2" : [0,1,0,1],
+            "y" : [0,0,0,1]
+        }
+    ETA = 0.3
+    EPOCHS = 10
+
+    main(data=AND, eta=ETA,epochs=EPOCHS,filename='and.model',plotfilename='and.png')
