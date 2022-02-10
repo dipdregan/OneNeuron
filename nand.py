f447eba83feaf09b8dc39cad6d986192d05c0f1e
Dipendra Pratap <dipendrapratap155@gmail.com>
2022-02-09 21:21:11 -0800

    creating nanda.py and its model

--- nand.py
+++ nand.py
@@ -1,1 +1,26 @@
- 
+from utils.model import Perceptron
+from utils.all_utils import prepare_data,save_model, save_plot
+import pandas as pd
+import numpy as np
+import joblib
+
+NAND ={
+    "x1" : [0,0,1,1],
+    "x2" : [0,1,0,1],
+    "y" : [1,1,1,0]
+}
+
+df = pd.DataFrame(NAND)
+
+X, y = prepare_data(df)
+
+ETA = 0.3
+EPOCHS = 10
+
+model  = Perceptron(eta= ETA, epochs= EPOCHS)
+model.fit(X,y)
+
+_= model.total_loss()
+
+save_model(model=model,filename='NAND.model')
+save_plot(df,'NAND.png',model )
