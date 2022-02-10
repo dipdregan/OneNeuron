dc2d478c4e25f3450fa79773edd19fb78efafbcb
Dipendra Pratap <dipendrapratap155@gmail.com>
2022-02-09 21:16:11 -0800

    organize the or.py

--- or.py
+++ or.py
@@ -4,23 +4,27 @@ import pandas as pd
 import numpy as np
 import joblib
 
-OR ={
-    "x1" : [0,0,1,1],
-    "x2" : [0,1,0,1],
-    "y" : [0,1,1,1]
-}
+def main():
+    OR ={
+        "x1" : [0,0,1,1],
+        "x2" : [0,1,0,1],
+        "y" : [0,1,1,1]
+    }
 
-df = pd.DataFrame(OR)
+    df = pd.DataFrame(OR)
 
-X, y = prepare_data(df)
+    X, y = prepare_data(df)
 
-ETA = 0.3
-EPOCHS = 10
+    ETA = 0.3
+    EPOCHS = 10
 
-model  = Perceptron(eta= ETA, epochs= EPOCHS)
-model.fit(X,y)
+    model  = Perceptron(eta= ETA, epochs= EPOCHS)
+    model.fit(X,y)
 
-_= model.total_loss()
+    _= model.total_loss()
 
-save_model(model=model,filename='OR.model')
-save_plot(df,'OR.png',model )
+    save_model(model=model,filename='OR.model')
+    save_plot(df,'OR.png',model )
+
+if __name__ =="__main__":
+    main()
