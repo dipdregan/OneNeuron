4804a899c877a61881c4236df345ed9ccde05d8b
Dipendra Pratap <dipendrapratap155@gmail.com>
2022-02-09 21:02:38 -0800

    first commit done AND and OR

---
+++ or.py
@@ -0,0 +1,26 @@
+from utils.model import Perceptron
+from utils.all_utils import prepare_data,save_model, save_plot
+import pandas as pd
+import numpy as np
+import joblib
+
+OR ={
+    "x1" : [0,0,1,1],
+    "x2" : [0,1,0,1],
+    "y" : [0,1,1,1]
+}
+
+df = pd.DataFrame(OR)
+
+X, y = prepare_data(df)
+
+ETA = 0.3
+EPOCHS = 10
+
+model  = Perceptron(eta= ETA, epochs= EPOCHS)
+model.fit(X,y)
+
+_= model.total_loss()
+
+save_model(model=model,filename='OR.model')
+save_plot(df,'OR.png',model )
